baf8130ab59370afe6a94e3aa276d00dd9fb7ec3
Lidia Bhogal <lidiapriano@lidias-iMac.local>
2023-01-29 15:32:59 +0000

    updated js

--- script.js
+++ script.js
@@ -1,44 +1,72 @@
 var today = moment();
-  $("#today forecast").text(today.format("dddd Do MMMM"));
+  $(".date").text(today.format('L'));
 
-  function variables (){
-    let cityName = document.getElementById("search-input");
-    let searchB = document.getElementById('search-button');
-    const clearEl = document.getElementById("clear-history");
-    let nameEl = document.getElementById("London");
-    let currentTempEl = document.getElementById("temperature");
-    let currentHumidityEl = document.getElementById("humidity");
-   let currentWindEl = document.getElementById("wind-speed");
-   let fivedayEl = document.getElementById("fiveday-header");
-   let todayweatherEl = document.getElementById("today-weather");
+  let root = 'https://api.openweathermap.org/data/2.5/weather?q='
+
+  let searchB = document.getElementById('search-button');
+  let cityName = document.getElementById("search-input");
+  let apiKey = "3M1VZ9NYFdkpcjDQgxUoGBwE0x7FILRI"
+  let wholeUrl = root+cityName+ "&limit=5&appid=" + apiKey;
+
+  $('#search-button').on('click', function(event){
+      event.preventDefault();
+      console.log(cityName.value);
+     searchB = ('search-button').val().trim();
+     $("#search-button").val('')
+    
+  
+  
+  })
+  $.ajax({
+    url: wholeUrl,
+    method: "GET"
+  }).then(function(response) {
+    let cityOption = document.getElementById("form-input weather-search")
+    let forcast = response.forcast;
+    let cityOptionanswer = $("<p>").text(forcast)
+    cityOption.append(cityOptionanswer)
+   })
+
+
+
+//   function variables (){
+//     let cityName = document.getElementById("search-input");
+//     let searchB = document.getElementById('search-button');
+//     const clearEl = document.getElementById("clear-history");
+//     let nameEl = document.getElementById("London");
+//     let currentTempEl = document.getElementById("temperature");
+//     let currentHumidityEl = document.getElementById("humidity");
+//    let currentWindEl = document.getElementById("wind-speed");
+//    let fivedayEl = document.getElementById("fiveday-header");
+//    let todayweatherEl = document.getElementById("today-weather");
   
 
 
  
   
-// // // beginning of url
-let root = 'http://api.openweathermap.org/geo/1.0/direct?q='
+// // // // beginning of url
+// let root = 'http://api.openweathermap.org/geo/1.0/direct?q='
 
 
-function getWeather (cityName){
-  // adding all variables together
-  let wholeUrl = root+cityName+ "&limit=5&appid=" + apiKey;
-  $.ajax({
-    url: wholeUrl,
-    method: "GET"
-  }).then(function(response) {
-    todayweatherEl.classlist.remove('done');
+// function getWeather (cityName){
+//   // adding all variables together
+//   let wholeUrl = root+cityName+ "&limit=5&appid=" + apiKey;
+//   $.ajax({
+//     url: wholeUrl,
+//     method: "GET"
+//   }).then(function(response) {
+//     todayweatherEl.classlist.remove('done');
 
-   const today = moment();
-  $("#today forecast").text(today.format("dddd Do MMMM"));
-  let weatherPic = response.data.weather[0].icon;
-  console.log(cityName.value)
-  })
-}
+//    const today = moment();
+//   $("#today forecast").text(today.format("dddd Do MMMM"));
+//   let weatherPic = response.data.weather[0].icon;
+//   console.log(cityName.value)
+//   })
+// }
 
-  }
+//   }
 
-  console.log(cityName)
+//   console.log(cityName)
 
 
 
