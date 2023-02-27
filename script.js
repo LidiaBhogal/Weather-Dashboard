66fcf1b75c141a980c35321bac2de27b84b409d7
Lidia Bhogal <lydiapriano@yahoo.co.uk>
2023-02-27 18:58:53 +0000

    updated js to include 5 day weather

--- script.js
+++ script.js
@@ -1,136 +1,72 @@
 var todaysDate = moment();
-  $(".date").text(today.format('L'));
+$(".date").text(todaysDate.format('L'));
 
-  let root = 'https://api.openweathermap.org/data/2.5/forecast?q='
+let root = 'https://api.openweathermap.org/data/2.5/forecast?q='
 
-  let searchB = document.getElementById('search-button');
-  let cityName = document.getElementById("search-input");
-  let apiKey = "699f44b239ac2b4ed6242e958b370589"
-  let wholeUrl = root+ 'london' + '&' + apiKey;
+let searchB = document.getElementById('search-button');
+let cityName = document.getElementById("search-input");
+let apiKey = "699f44b239ac2b4ed6242e958b370589"
+let wholeUrl = root + 'london' + '&' + apiKey;
 
-  $('#search-button').on('click', function(event){
-      event.preventDefault();
-      console.log(cityName.value);
-     
-     $("#search-button").val('')
-    
-  
-  
-  })
-  console.log(cityName);
-
-fetch(wholeUrl).then(response => response.json()).then(data => console.log(data));
+console.log(document.querySelector("#forecast"))
+let selectedCity = document.querySelector("#selected-city-heading")
+let cityTemp = document.querySelector('#selected-city-temp')
+let cityWind = document.querySelector("#selected-city-wind")
+let cityHumidity = document.querySelector('#selected-city-humidity')
+let weatherIcon = document.querySelector('#todays-icon')
+let dayWeather = document.querySelectorAll('.card-body')
 
 
-localStorage.setItem("selectedCity", cityName);
-$("#selected-city-heading").text(cityName + " " + "(" + todaysDate + ")");
+$('#search-form').on('submit', function (event) {
+  event.preventDefault();
+  console.log(cityName.value);
 
+  $("#search-button").val('')
+  let todaysWeather = `https://api.openweathermap.org/data/2.5/weather?q=${cityName.value}&units=metric&appid=${apiKey}`
 
+  fetch(todaysWeather)
+    .then(preJSON => {
+      console.log(preJSON);
+      return preJSON.json()
+    })
+    .then(currentWeather => {
+      console.log("LINE 33", currentWeather)
+      selectedCity.textContent = currentWeather.name
+      cityTemp.textContent = currentWeather.main.temp
+      cityWind.textContent = currentWeather.wind.speed
+      cityHumidity.textContent = currentWeather.main.humidity
+      weatherIcon.textContent = currentWeather.weather.icon
+    })
+    .catch(err => console.error(err));
 
-var todaysTemp = (Math.floor((response.list[0].main.temp) -273.15));
-localStorage.setItem("todaysTemp", todaysTemp);
-$("#selected-city-temp").text("Temp: " + todaysTemp + "°C");
+  let day1 = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName.value}&units=metric&appid=${apiKey}`
 
+  fetch(day1)
+    .then(beforeJson => {
+      // console.log(beforeJson);
+      return beforeJson.json()
+    })
+    .then(response => {
+      // console.log("LINE 50", response)
+      for (let i = 0; i < 6; i++) {
+        console.log(response.list[i * 8])
+        console.log(dayWeather[i].children)
+        dayWeather[i].children[1].textContent = response.list[i * 8].main.temp + " C"
+        dayWeather[i].children[2].textContent = response.list[i * 8].wind.speed
+        dayWeather[i].children[3].textContent = response.list[i * 8].main.humidity
+        dayWeather[i].children[0].setAttribute('src',`https://openweathermap.org/img/wn/${response.list[i*8].weather[0].icon}@2x.png`)
+        // console.log(dayWeather[i].children)
 
-var todaysWind = response.list[0].wind.speed;
-localStorage.setItem("todaysWind", todaysWind);
-$("#selected-city-wind").text("Wind: " + todaysWind + " KPH");
+      }
+    })
+    .catch(err => console.error(err));
+})
 
-
-
-var todaysHumidity = response.list[0].main.humidity;
-localStorage.setItem("todaysHumidity", todaysHumidity);
-$("#selected-city-humidity").text(" Humidity: " + todaysHumidity + " %");
-
-
-  // $.ajax({
-  //   url: wholeUrl,
-  //   method: "GET"
-  // }).then(function(response) {
-  //   let cityOption = document.getElementById("form-input weather-search")
-  //   let forcast = response.forcast;
-  //   let cityOptionanswer = $("<p>").text(forcast)
-  //   cityOption.append(cityOptionanswer)
-  //   console.log(forcast);
-  //  })
-
-
-
-//   function variables (){
-//     let cityName = document.getElementById("search-input");
-//     let searchB = document.getElementById('search-button');
-//     const clearEl = document.getElementById("clear-history");
-//     let nameEl = document.getElementById("London");
-//     let currentTempEl = document.getElementById("temperature");
-//     let currentHumidityEl = document.getElementById("humidity");
-//    let currentWindEl = document.getElementById("wind-speed");
-//    let fivedayEl = document.getElementById("fiveday-header");
-//    let todayweatherEl = document.getElementById("today-weather");
-  
-
-
- 
-  
-// // // // beginning of url
-// let root = 'http://api.openweathermap.org/geo/1.0/direct?q='
-
-
-// function getWeather (cityName){
-//   // adding all variables together
-//   let wholeUrl = root+cityName+ "&limit=5&appid=" + apiKey;
-//   $.ajax({
-//     url: wholeUrl,
-//     method: "GET"
-//   }).then(function(response) {
-//     todayweatherEl.classlist.remove('done');
-
-//    const today = moment();
-//   $("#today forecast").text(today.format("dddd Do MMMM"));
-//   let weatherPic = response.data.weather[0].icon;
-//   console.log(cityName.value)
-//   })
-// }
-
-//   }
-
-//   console.log(cityName)
+// console.log(cityName)
 
 
 
 
 
-
-
-
-
-  
-// // search button
-// let searchB = document.getElementById('search-button');
- 
-
-// $('#search-button').on('click', function(event){
-//     event.preventDefault();
-//     console.log(cityName.value);
-   
-// console.log(cityName);
-
-// })
-
-// let todaysWeather = document.getElementById('today forecast');
-// todaysWeather.classList
-
-
-//  function getWeather (){
-//   let place = $(this).attr('search-input')
-//   const queryURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + place + "3M1VZ9NYFdkpcjDQgxUoGBwE0x7FILRI";
-
-
-//   $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   }).then(function(response) {
-
-//   }
-//  )}
 
 
